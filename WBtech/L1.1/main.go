74c4a1dcec289383fad274ed79b5293e1c3723f8
Viki <gerrenka@MacBook-Air-Viktory.local>
2024-12-24 22:36:34 +0300

    some minor changes

--- WBtech/L1.1/main.go
+++ WBtech/L1.1/main.go
@@ -4,23 +4,22 @@ import (
 	"fmt"
 	)
 
-	type Human struct{
-		name string
-		age int
-	}
+type Human struct{
+	name string
+	age int
+}
 
-	type Action struct{
-		Human
-		id int
-	}
+type Action struct{
+	Human
+	id int
+}
 
-	func (h Human) do(){
-		fmt.Println(  "я здесь")
-	}
+func (h Human) do(){
+	fmt.Println("I'm", h.name, "and I'm", h.age, "years old.")
+}
 
 func main() {
 	Action := Action{Human{"Ivan", 25}, 1}
-
 	Action.do()
 }
 
